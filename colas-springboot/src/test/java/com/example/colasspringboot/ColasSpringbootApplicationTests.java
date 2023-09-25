64543e82baa2777ecacda0a4255919b753105754
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-24 22:51:38 -0500

    Creación proyecto spring boot

---
+++ colas-springboot/src/test/java/com/example/colasspringboot/ColasSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colasspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColasSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
