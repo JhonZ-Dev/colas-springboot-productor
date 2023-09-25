91c65cce822f67613e3870f469d206ba334db012
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-24 22:56:25 -0500

    Update ConfigColas.java

--- colas-springboot/src/main/java/com/example/colasspringboot/config/ConfigColas.java
+++ colas-springboot/src/main/java/com/example/colasspringboot/config/ConfigColas.java
@@ -1,9 +1,6 @@
 package com.example.colasspringboot.config;
 
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -47,5 +44,43 @@ public class ConfigColas {
     @Bean
     public TopicExchange topicExchange() {
         return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    //Bindign
+    @Bean
+    public Binding bindingBomnberos(Queue colaBomberosJZ, DirectExchange exchange) {
+        return BindingBuilder.bind(colaBomberosJZ).to(exchange).with(ROUTING_A);
+    }
+    @Bean
+    public Binding bindingAmbulancia(Queue colaAmbulanciaJZ, DirectExchange exchange) {
+        return BindingBuilder.bind(colaAmbulanciaJZ).to(exchange).with(ROUTING_B);
+    }
+    @Bean
+    public Binding bindingPolicia(Queue colaPoliciaJZ, DirectExchange exchange) {
+        return BindingBuilder.bind(colaPoliciaJZ).to(exchange).with(ROUTING_C);
+    }
+
+    @Bean
+    public Binding bindingBomberosTodos(Queue colaBomberosJZ, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(colaBomberosJZ).to(fanoutExchange);
+    }
+    @Bean
+    public Binding bindingAmbulanciaTodos(Queue colaAmbulanciaJZ, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(colaAmbulanciaJZ).to(fanoutExchange);
+    }
+    @Bean
+    public Binding bindingPoliciaTodos(Queue colaPoliciaJZ, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(colaPoliciaJZ).to(fanoutExchange);
+    }
+
+
+    @Bean
+    public Binding bindingBomberosIncendio(Queue colaBomberosJZ, TopicExchange topicExchange) {
+        return BindingBuilder.bind(colaBomberosJZ).to(topicExchange).with("bomberos.*");
+    }
+
+    @Bean
+    public Binding bindingAmbulanciasIncendio(Queue colaAmbulanciaJZ, TopicExchange topicExchange) {
+        return BindingBuilder.bind(colaAmbulanciaJZ).to(topicExchange).with("ambulancias.*");
     }
 }
