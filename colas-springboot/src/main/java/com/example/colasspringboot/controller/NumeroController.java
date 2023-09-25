20168269b702e1f3bb8b7338919f713cfa88550c
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-24 23:05:48 -0500

    Update NumeroController.java

--- colas-springboot/src/main/java/com/example/colasspringboot/controller/NumeroController.java
+++ colas-springboot/src/main/java/com/example/colasspringboot/controller/NumeroController.java
@@ -1,9 +1,20 @@
 package com.example.colasspringboot.controller;
 
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/emergencias-insertar")
 public class NumeroController {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private DirectExchange exchange;
+    @Autowired
+    private FanoutExchange fanoutExchange;
 }
