5135d4a444aa531a2955ada6d0394e21895b09a1
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-24 22:55:05 -0500

    Update ConfigColas.java

--- colas-springboot/src/main/java/com/example/colasspringboot/config/ConfigColas.java
+++ colas-springboot/src/main/java/com/example/colasspringboot/config/ConfigColas.java
@@ -10,4 +10,6 @@ public class ConfigColas {
     public static final String ROUTING_A = "bomberosJZ";
     public static final String ROUTING_B = "ambulanciasJZ";
     public static final String ROUTING_C = "policiaJZ";
+
+    private static final String EXCHANGE_NAME = "exchange-topic";
 }
